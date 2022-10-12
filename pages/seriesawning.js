a57d6c547ba82d54020a8dd6aa96619e0f6a583d
kmal808 <kjmalley7@gmail.com>
2022-10-11 20:42:27 -0700

    refactor: adding head, nav, and footer to layout

--- pages/seriesawning.js
+++ pages/seriesawning.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import Head from 'next/head'
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
 import Nav from '../components/Nav'
 
 const SeriesAwning = () => {
+	useEffect(() => {
+		themeChange(false)
+	}, [])
+
 	return (
 		<div>
+			<Head>
+				<title>CPQ</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta
+					name='description'
+					content='Configure price quote for window and door contators'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 			<Nav />
+			<div></div>
+			<Footer />
 		</div>
 	)
 }
