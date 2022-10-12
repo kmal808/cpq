31e9c46adc6e3ac070e82a299db63025fb668713
kmal808 <kjmalley7@gmail.com>
2022-10-11 20:48:18 -0700

    task: creating page layout with head, nav, and footer

--- pages/seriesglider.js
+++ pages/seriesglider.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import Head from 'next/head'
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
+import Footer from '../components/Footer'
 import Nav from '../components/Nav'
 
 const SeriesGlider = () => {
+	useEffect(() => {
+		themeChange(false)
+	}, [])
+
 	return (
-		<div className='container min-h-screen mx-auto px-4'>
+		<div>
+			<Head>
+				<title>CPQ</title>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta
+					name='description'
+					content='Configure price quote for window and door contators'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 			<Nav />
-			Series Glider
+			<div></div>
+			<Footer />
 		</div>
 	)
 }
